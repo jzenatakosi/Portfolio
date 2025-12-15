bb37cfb7a25a23c6244d3f9be2fde7fd8c2d3145
jZenatakosi <78544069+jzenatakosi@users.noreply.github.com>
2025-12-15 19:07:02 +0300

    Fixed Databse name forpythonanywhere

--- sitecore/settings.py
+++ sitecore/settings.py
@@ -42,9 +42,14 @@ WSGI_APPLICATION = "sitecore.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("DB_NAME", "myportfolio"),
+        # Add username prefix only if DB_HOST contains 'pythonanywhere'
+        "NAME": (
+            os.getenv("DB_USER", "Admin") + "$" + os.getenv("DB_NAME", "myportfolio")
+            if "pythonanywhere" in os.getenv("DB_HOST", "")
+            else os.getenv("DB_NAME", "myportfolio")
+        ),
         "USER": os.getenv("DB_USER", "Admin"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),  # No default for security!
+        "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST", "localhost"),
         "PORT": os.getenv("DB_PORT", "3306"),
         "OPTIONS": {
